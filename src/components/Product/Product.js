913e7e9bc0102f2dece7b2551697ce84c09ddb30
istiakabir~ <istiakabirtrofder@gmail.com>
2022-03-25 13:54:31 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    // console.log(props.allProduct)
+    const {name, seller, price, ratings, img, quantity} = props.allProduct;
+    return (
+        <div className='product'>
+        <img src={img} alt="" />
+           <div className='product-info'>
+                <h3 className='product-name'>{name}</h3>
+                <h4>Price: ${price}</h4>
+                <p><small>Manufacturer : {seller}</small></p>
+                <p><small>Rating : {ratings} star</small></p>
+           </div>
+            <button onClick={() => props.handleToCart(props.allProduct)} className='product-cart'>
+                Add to Cart
+            </button>
+            
+        </div>
+    );
+};
+
+export default Product;
